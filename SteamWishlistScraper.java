1f73b2f6f68fc22687fa639fb21d98190e96b167
Tim Haskins <timothyjhask@gmail.com>
2016-03-16 18:39:28 -0400

    Added feature allowing for either id or profile in arguments

--- SteamWishlistScraper.java
+++ SteamWishlistScraper.java
@@ -3,6 +3,18 @@ import java.io.*;
 import org.jsoup.*;
 import org.jsoup.nodes.*;
 import org.jsoup.select.*;
+// org.jsoup does not come standard in the Java library
+// It can be found here as a zip file: (unzips to reveal a jar file)
+// http://jsoup.org/download
+// Last checked March 16, 2016
+
+/*
+
+ *Expected arguments:
+ *args[0]: The name of the text file that this program will print to.
+ *args[1]: The ID or profile of the steam user in question.
+            Example: 76561198099289598, Johnny\ Carlo
+ */
 
 public class SteamWishlistScraper
 {
@@ -10,20 +22,39 @@ public class SteamWishlistScraper
    {
       String id = args[1];
       String url = "http://steamcommunity.com/profiles/" + id + "/wishlist";
-      System.out.print(url + "\n");
+      String url2 = "http://steamcommunity.com/id/" + id + "/wishlist";
+      System.out.print("Trying " + url + "\n");
       
       Document doc = Jsoup.parse("<html></html>");
       try
       {
          doc = Jsoup.connect(url).get();
+         if( doc.title( ).equals( "Steam Community :: Error" ) )
+         {
+            System.out.print("Didn't work. Trying " + url2 + "\n");
+            doc = Jsoup.connect(url2).get();
+            if( doc.title( ).equals( "Steam Community :: Error" ) )
+            {
+               System.out.print("Neither worked. Exiting...\n");
+               return;
+            }
+         }
+         //System.out.print(doc.title());
+         if( doc.title().indexOf("::") == doc.title().lastIndexOf("::") )
+         {
+            System.out.print( "Looks like the account is private. Exiting...\n" );
+            return;
+         }
+         System.out.print("Got it!\n");
+         
       }
       catch(IOException e){}
       
-      //System.out.print(doc.title() + "\n");
       Element thing = doc.getElementById("wishlist_items");
-      //System.out.print(thing.text() + "\n");
+      
       String out = parseThrough(thing);
       writeOut(out, args[0]);
+      System.out.print("Done.\n");
    }
    
    public static String parseThrough(Element in)
@@ -33,13 +64,9 @@ public class SteamWishlistScraper
       
       for(Element item : items)
       {
-         //System.out.print(item.getElementsByClass("ellipsis").first().text() + " ... ");
          give += item.getElementsByClass("ellipsis").first().text() + " ... ";
          if(item.getElementsByClass("price").first() == null)
          {
-            //System.out.print(item.getElementsByClass("discount_pct").first().text() + " off " 
-            //   + item.getElementsByClass("discount_original_price").first().text() + " = " 
-            //   + item.getElementsByClass("discount_final_price").first().text() + "\n");
             give += 
                  item.getElementsByClass("discount_pct").first().text()            + " off " 
                + item.getElementsByClass("discount_original_price").first().text() + " = " 
@@ -47,11 +74,10 @@ public class SteamWishlistScraper
          }
          else
          {
-            //System.out.print(item.getElementsByClass("price").first().text() + "\n");
             give += item.getElementsByClass("price").first().text() + "\n";
          }
       }
-      System.out.println(give);
+      //System.out.println(give);
       return give;
    }
    
